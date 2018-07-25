16dfb5f6dd860481d0143e45a1ccf132e4ce2b06
starc0unter <79835070975@yandex.ru>
2018-07-25 09:34:22 +0700

    Simple chat, version 0.7: 1. Added auth window; 2. Now users can send private messages

---
+++ SimpleChat/src/main/java/ru/chentsov/javacore/ChatConstants.java
@@ -0,0 +1,20 @@
+package ru.chentsov.javacore;
+
+/**
+ * @author Evgenii Chentsov
+ */
+public final class ChatConstants {
+
+    public static final String TERMINATE_CONNECTION_FLAG = "/end ";
+    public static final String PRIVATE_MESSAGE_FLAG = "/w ";
+    public static final String AUTHORIZE_OK_FLAG = "/authOK ";
+    public static final String AUTHORIZE_FLAG = "/auth ";
+    public static final String REGISTER_FLAG = "/newClient ";
+    public static final String REGISTER_OK_FLAG = "/newClientOK ";
+    public static final String CHAT_ENTERED_FLAG = " entered the chat";
+    public static final String FROM = "from ";
+    public static final String FROM_PRIVATE = "private from ";
+
+    private ChatConstants() {};
+
+}
